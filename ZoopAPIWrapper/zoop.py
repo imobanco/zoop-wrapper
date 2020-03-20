65df58506b38856158bb30344ebeabc65ef4e937
rodrigondec <rodrigondec@gmail.com>
2020-03-20 17:09:42 -0300

    zoop.__ini__ ok

--- ZoopAPIWrapper/zoop.py
+++ ZoopAPIWrapper/zoop.py
@@ -1,17 +1,19 @@
-import requests
 import json
+
+import requests
+
 from ZoopAPIWrapper.constants import ZOOP_KEY, MAIN_SELLER, MARKETPLACE_ID
 
 
-class Zoop():
+class Zoop:
     """This class wraps the zoop API to ease use in python
 
     """
-    def __init__(self, marketplace_id, publishable_key):
-        self.BASE_ZOOP_URL = 'https://api.zoop.ws/v1/marketplaces/'
+    def __init__(self, ):
+        self._base_url = 'https://api.zoop.ws/v1/marketplaces'
 
-        self.marketplace_id = marketplace_id
-        self.basic_auth = publishable_key
+        self.__marketplace_id = MARKETPLACE_ID
+        self.__key = ZOOP_KEY
 
     def search_seller_by_id(self, identifier):
         url = self.BASE_ZOOP_URL + f'{MARKETPLACE_ID}/sellers/{MAIN_SELLER}'

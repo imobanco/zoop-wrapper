ea942ca3423e01996dabce7211ade8a273af2ddc
rodrigondec <rodrigondec@gmail.com>
2020-03-23 16:26:13 -0300

    test seller updated

--- tests/models/test_seller.py
+++ tests/models/test_seller.py
@@ -1,10 +1,19 @@
 from unittest import TestCase
 
-from ZoopAPIWrapper.models import Seller
+from ZoopAPIWrapper.models import Seller, BusinessSeller, IndividualSeller, Owner, Address
 
 
 class SellerTestCase(TestCase):
-    def test_from_dict(self):
+    def test_get_class_from_type(self):
+        individual_klass = Seller.get_seller_class('individual')
+        self.assertEqual(individual_klass, IndividualSeller)
+
+        business_klass = Seller.get_seller_class('business')
+        self.assertEqual(business_klass, BusinessSeller)
+
+        self.assertRaises(ValueError, Seller.get_seller_class, 'test')
+
+    def test_seller_from_dict_business(self):
         data = {
             'id': 'foo',
             'resource': 'foo',
@@ -16,7 +25,7 @@ class SellerTestCase(TestCase):
             'marketplace_id': 'foo',
 
             "status": 'foo',
-            "type": 'foo',
+            "type": 'business',
             "account_balance": 'foo',
             "current_balance": 'foo',
             "description": 'foo',
@@ -31,16 +40,59 @@ class SellerTestCase(TestCase):
             "default_debit": 'foo',
             "default_credit": 'foo',
             "merchant_code": 'foo',
-            "terminal_code": 'foo'
+            "terminal_code": 'foo',
+
+            "business_name": "foo",
+            "business_phone": "foo",
+            "business_email": "foo",
+            "business_website": "foo",
+            "business_description": "foo",
+            "business_opening_date": "foo",
+            "business_facebook": "foo",
+            "business_twitter": "foo",
+            "ein": "foo",
+            "business_address": {
+                "line1": "foo",
+                "line2": "foo",
+                "line3": "foo",
+                "neighborhood": "foo",
+                "city": "foo",
+                "state": "foo",
+                "postal_code": "foo",
+                "country_code": "foo"
+            },
+            "owner": {
+                "first_name": "foo",
+                "last_name": "foo",
+                "email": "foo",
+                "phone_number": "foo",
+                "taxpayer_id": "foo",
+                "birthdate": "foo",
+                "address": {
+                    "line1": "foo",
+                    "line2": "foo",
+                    "line3": "foo",
+                    "neighborhood": "foo",
+                    "city": "foo",
+                    "state": "foo",
+                    "postal_code": "foo",
+                    "country_code": "foo"
+                }
+            }
         }
         instance = Seller.from_dict(data)
 
-        self.assertIsInstance(instance, Seller)
+        self.assertIsInstance(instance, BusinessSeller)
         self.assertEqual(instance.id, 'foo')
         self.assertEqual(instance.marketplace_id, 'foo')
-        self.assertEqual(instance.type, 'foo')
+        self.assertEqual(instance.type, 'business')
+        self.assertEqual(instance.business_name, 'foo')
+        self.assertIsInstance(instance.business_address, Address)
+        self.assertEqual(instance.business_address.city, 'foo')
+        self.assertIsInstance(instance.owner, Owner)
+        self.assertEqual(instance.owner.first_name, 'foo')
 
-    def test_to_dict(self):
+    def test_seller_from_dict_individual(self):
         data = {
             'id': 'foo',
             'resource': 'foo',
@@ -52,7 +104,7 @@ class SellerTestCase(TestCase):
             'marketplace_id': 'foo',
 
             "status": 'foo',
-            "type": 'foo',
+            "type": 'individual',
             "account_balance": 'foo',
             "current_balance": 'foo',
             "description": 'foo',
@@ -67,9 +119,34 @@ class SellerTestCase(TestCase):
             "default_debit": 'foo',
             "default_credit": 'foo',
             "merchant_code": 'foo',
-            "terminal_code": 'foo'
+            "terminal_code": 'foo',
+
+            "first_name": "foo",
+            "last_name": "foo",
+            "email": "foo",
+            "taxpayer_id": "foo",
+            "phone_number": "foo",
+            "birthdate": 'foo',
+            "website": 'foo',
+            "facebook": 'foo',
+            "twitter": 'foo',
+            "address": {
+                "line1": "Avenida Senador Casimiro da Rocha",
+                "line2": "1000",
+                "line3": "de 0741/742 a 1095/1096",
+                "neighborhood": "Mirand\u00f3polis",
+                "city": "foo",
+                "state": "SP",
+                "postal_code": "04047002",
+                "country_code": "BR"
+            },
         }
         instance = Seller.from_dict(data)
 
-        self.assertIsInstance(instance, Seller)
-        self.assertEqual(instance.to_dict(), data)
+        self.assertIsInstance(instance, IndividualSeller)
+        self.assertEqual(instance.id, 'foo')
+        self.assertEqual(instance.marketplace_id, 'foo')
+        self.assertEqual(instance.type, 'individual')
+        self.assertEqual(instance.first_name, 'foo')
+        self.assertIsInstance(instance.address, Address)
+        self.assertEqual(instance.address.city, 'foo')

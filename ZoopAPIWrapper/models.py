5252b5bcbb3a778649d71068f8ce98452c78fdc2
rodrigondec <rodrigondec@gmail.com>
2020-03-23 16:25:10 -0300

    functions to get instances from resource

--- ZoopAPIWrapper/models.py
+++ ZoopAPIWrapper/models.py
@@ -1,4 +1,21 @@
 
+def _get_model_class_from_resource(resource):
+    if resource == Seller.RESOURCE:
+        return Seller
+    else:
+        raise ValueError(f'model não identificado para resource {resource}!')
+
+
+def get_instance_from_data(data):
+    resource = data.get('resource')
+
+    try:
+        klass = _get_model_class_from_resource(resource)
+        return klass.from_dict(data)
+    except ValueError:
+        return None
+
+
 class ZoopBase:
     __FIELDS = []
 

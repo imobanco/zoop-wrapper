b16de3f13aa753e1d360ce0b375e126e269e8ec6
rodrigondec <rodrigondec@gmail.com>
2020-03-24 16:54:33 -0300

    LOG_LEVEL config variable

--- ZoopAPIWrapper/constants.py
+++ ZoopAPIWrapper/constants.py
@@ -4,3 +4,5 @@ from decouple import config
 ZOOP_KEY = config('ZOOP_KEY')
 MARKETPLACE_ID = config('MARKETPLACE_ID')
 MAIN_SELLER = config('MAIN_SELLER')
+
+LOG_LEVEL = config('LOG_LEVEL', cast=int)

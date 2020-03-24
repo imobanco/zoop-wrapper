246f8ee1d49c39086fb3ea6f15f172d982ea2645
rodrigondec <rodrigondec@gmail.com>
2020-03-24 17:46:55 -0300

    base docstrings

--- ZoopAPIWrapper/models/base.py
+++ ZoopAPIWrapper/models/base.py
@@ -1,15 +1,45 @@
 class ZoopBase:
+    """
+    This class represent a bare ZoopBase object.
+    A instance of this class doesn't have attributes.
+
+    This class has the attribute __FIELDS with the list of attributes it has.
+    The purpose of this is to construct the dict of the object.
+    """
     __FIELDS = []
 
     def __init__(self, *args, **kwargs):
+        """
+        constructor
+
+        Args:
+            *args:
+            **kwargs:
+        """
         pass
 
     @classmethod
     def _from_dict(cls, **entries):
+        """
+        construct a instance of this class from **entries
+
+        Args:
+            **entries:
+
+        Returns: instance initialized of class
+        """
         return cls(**entries)
 
     @classmethod
     def from_dict(cls, data):
+        """
+        construct a instance of this class from dict
+
+        Args:
+            data: dict of data
+
+        Returns: instance initialized of class or None
+        """
         try:
             return cls._from_dict(**data)
         except TypeError as e:
@@ -17,24 +47,51 @@ class ZoopBase:
             return None
 
     def to_dict(self):
+        """
+        serialize the instance to dict
+        Returns: dict of instance
+        """
         data = {}
         for field in self.fields:
             try:
-                attr = self.__getattribute__(field).to_dict()
+                """our attr may be a ZoopBase instance. 
+                Let's try to get its serialized value!"""
+                attr = getattr(self, field).to_dict()
             except AttributeError:
-                attr = self.__getattribute__(field)
+                """our attr doesn't have to_dict() method. 
+                Oh snap! It's not a ZoopBase instance!"""
+                attr = getattr(self, field)
 
             if attr is not None:
+                """only serialize values which are not None"""
                 data[field] = attr
 
         return data
 
     @property
     def fields(self):
+        """
+        the fields of ZoopBase are it's __FIELDS attrs.
+        it's important to be a new list (high order function)
+        Returns: new list of attributes
+        """
         return list(self.__FIELDS)
 
 
 class ZoopModel(ZoopBase):
+    """
+    This class and it's subclasses have attributes.
+    The __FIELDS list the attributes this class
+    has responsability of constructing in the serialization to dict.
+
+    Attributes:
+        id: identifier string
+        resource: type string
+        uri: uri string
+        created_at: date of creation
+        updated_at: date of update
+        metadata: dict with metadata
+    """
     __FIELDS = ["id", "resource", "uri", "created_at", "updated_at", "metadata"]
 
     def __init__(self, id=None, resource=None, uri=None,
@@ -51,12 +108,25 @@ class ZoopModel(ZoopBase):
 
     @property
     def fields(self):
+        """
+        the fields of ZoopBase are it's __FIELDS extended with it's father fields.
+        it's important to be a new list (high order function)
+        Returns: new list of attributes
+        """
         super_fields = super().fields
         super_fields.extend(self.__FIELDS)
         return list(super_fields)
 
 
 class ZoopMarketPlaceModel(ZoopModel):
+    """
+    This class and it's subclasses have attributes.
+    The __FIELDS list the attributes this class
+    has responsability of constructing in the serialization to dict.
+
+    Attributes:
+        marketplace_id: identifier string
+    """
     __FIELDS = ["marketplace_id"]
 
     def __init__(self, marketplace_id=None, **kwargs):
@@ -66,12 +136,32 @@ class ZoopMarketPlaceModel(ZoopModel):
 
     @property
     def fields(self):
+        """
+        the fields of ZoopBase are it's __FIELDS extended with it's father fields.
+        it's important to be a new list (high order function)
+        Returns: new list of attributes
+        """
         super_fields = super().fields
         super_fields.extend(self.__FIELDS)
         return list(super_fields)
 
 
 class Address(ZoopBase):
+    """
+    This class and it's subclasses have attributes.
+    The __FIELDS list the attributes this class
+    has responsability of constructing in the serialization to dict.
+
+    Attributes:
+        line1: complete street name
+        line2: number
+        line3: comlement
+        neighborhood: neighborhood
+        city: city
+        state: Código ISO 3166-2 para o estado
+        postal_code: psotal code
+        country_code: ISO 3166-1 alpha-2 - códigos de país de duas letras
+    """
     __FIELDS = ["line1", "line2", "line3",
                 "neighborhood", "city", "state",
                 "postal_code", "country_code"]
@@ -92,6 +182,11 @@ class Address(ZoopBase):
 
     @property
     def fields(self):
+        """
+        the fields of ZoopBase are it's __FIELDS extended with it's father fields.
+        it's important to be a new list (high order function)
+        Returns: new list of attributes
+        """
         super_fields = super().fields
         super_fields.extend(self.__FIELDS)
         return list(super_fields)

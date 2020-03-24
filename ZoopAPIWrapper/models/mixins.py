bfd257a311cfa442b3ce519f81a4b3d10bfa31b1
rodrigondec <rodrigondec@gmail.com>
2020-03-24 14:33:28 -0300

    BusinessOrIndividualMixin created

---
+++ ZoopAPIWrapper/models/mixins.py
@@ -0,0 +1,43 @@
+from ZoopAPIWrapper.utils import classproperty
+
+
+class BusinessOrIndividualMixin:
+    BUSINESS_IDENTIFIER = 'ein'
+    INDIVIDUAL_IDENTIFIER = 'taxpayer_id'
+
+    # noinspection PyMethodParameters
+    @classproperty
+    def business_class(cls):
+        raise NotImplementedError()
+
+    # noinspection PyMethodParameters
+    @classproperty
+    def individual_class(cls):
+        raise NotImplementedError()
+
+    @classmethod
+    def __extract_identifier(cls, data):
+        has_individual_identifier = data.get(cls.INDIVIDUAL_IDENTIFIER, False)
+        has_business_identifier = data.get(cls.BUSINESS_IDENTIFIER, False)
+
+        if ((not has_business_identifier and not has_individual_identifier) or
+                (has_business_identifier and has_individual_identifier)):
+            raise TypeError(f'missing identifier. Must be either "{cls.INDIVIDUAL_IDENTIFIER}" or '
+                            f'"{cls.BUSINESS_IDENTIFIER}"')
+        elif has_individual_identifier:
+            identifier_type = cls.INDIVIDUAL_IDENTIFIER
+        else:
+            identifier_type = cls.BUSINESS_IDENTIFIER
+
+        return identifier_type
+
+    @classmethod
+    def get_class(cls, data):
+        identifier_type = cls.__extract_identifier(data)
+
+        if identifier_type == cls.INDIVIDUAL_IDENTIFIER:
+            return cls.individual_class
+        elif identifier_type == cls.BUSINESS_IDENTIFIER:
+            return cls.business_class
+        else:
+            raise ValueError('costumer_identifier type not identified')
---
+++ tests/models/test_mixins.py
@@ -0,0 +1,68 @@
+from unittest import TestCase
+from unittest.mock import patch, MagicMock
+
+from ZoopAPIWrapper.models.mixins import BusinessOrIndividualMixin
+
+
+class BusinessOrIndividualTestCase(TestCase):
+    def test_extract_identifier_missing(self):
+        data = {}
+
+        self.assertRaises(
+            TypeError,
+            BusinessOrIndividualMixin.
+                _BusinessOrIndividualMixin__extract_identifier,
+            data
+        )
+
+    def test_extract_identifier_both(self):
+        data = {
+            'taxpayer_id': 'foo',
+            'ein': 'foo'
+        }
+
+        self.assertRaises(
+            TypeError,
+            BusinessOrIndividualMixin.
+                _BusinessOrIndividualMixin__extract_identifier,
+            data
+        )
+
+    def test_extract_identifier_individual(self):
+        data = {
+            'taxpayer_id': 'foo'
+        }
+
+        indentifier_type = BusinessOrIndividualMixin._BusinessOrIndividualMixin__extract_identifier(data)
+        self.assertEqual(indentifier_type, 'taxpayer_id')
+
+    def test_extract_identifier_business(self):
+        data = {
+            'ein': 'foo'
+        }
+
+        indentifier_type = BusinessOrIndividualMixin._BusinessOrIndividualMixin__extract_identifier(data)
+        self.assertEqual(indentifier_type, 'ein')
+    
+    @patch('ZoopAPIWrapper.models.mixins.BusinessOrIndividualMixin.individual_class')
+    def test_get_class_individual(self, mocked_class):
+        self.assertIsInstance(mocked_class, MagicMock)
+
+        data = {
+            'taxpayer_id': 'foo'
+        }
+        
+        klass = BusinessOrIndividualMixin.get_class(data)
+        self.assertEqual(klass, mocked_class)
+
+    @patch('ZoopAPIWrapper.models.mixins.BusinessOrIndividualMixin.business_class')
+    def test_get_class_business(self, mocked_class):
+        self.assertIsInstance(mocked_class, MagicMock)
+
+        data = {
+            'ein': 'foo'
+        }
+
+        klass = BusinessOrIndividualMixin.get_class(data)
+        self.assertEqual(klass, mocked_class)
+

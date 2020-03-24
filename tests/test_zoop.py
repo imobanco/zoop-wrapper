179e7668ff7b765dfef6cc511bfeb086c1b62c79
rodrigondec <rodrigondec@gmail.com>
2020-03-24 14:47:47 -0300

    test failing

--- tests/test_zoop.py
+++ tests/test_zoop.py
@@ -121,20 +121,24 @@ class ZoopTestCase(TestCase):
             'email': 'foo@bar.com',
             'phone_number': '+55 84 99999-9999',
             'birthdate': '1994-12-27',
-
-            # 'address': {
-            #     'line1': 'foo',
-            #     'line2': '123',
-            #     'line3': 'barbar',
-            #     'neighborhood': 'fooofoo',
-            #     'city': 'Natal',
-            #     'state': 'BR-RN',
-            #     'postal_code': '59152250',
-            #     'country_code': "BR"
-            # }
         }
 
-        response = self.zoop.add_individual_seller(data)
+        response = self.zoop.add_seller(data)
+        self.assertEqual(response.status_code, 201, msg=response.error)
+
+    @patch('ZoopAPIWrapper.api.requests.post')
+    def test_add_business_seller(self, mocked_post):
+        mocked_post.return_value = MagicMock(content='{}', status_code=201)
+
+        data = {
+            "ein": gen.cnpj(),
+            'business_name': 'foo',
+            'business_phone': 'foo',
+            'business_email': 'foo',
+            'business_website': 'foo'
+        }
+
+        response = self.zoop.add_seller(data)
         self.assertEqual(response.status_code, 201, msg=response.error)
 
     def test_add_individual_seller_duplicated(self):
@@ -164,7 +168,7 @@ class ZoopTestCase(TestCase):
             # }
         }
 
-        response = self.zoop.add_individual_seller(data)
+        response = self.zoop.add_seller(data)
         self.assertEqual(response.status_code, 409, msg=response.data)
 
     @patch('ZoopAPIWrapper.api.requests.delete')

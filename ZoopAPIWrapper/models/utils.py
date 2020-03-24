ba5f31a67ad5ea2682314bd17207aabefbb78713
rodrigondec <rodrigondec@gmail.com>
2020-03-24 18:14:00 -0300

    utils docstrings

--- ZoopAPIWrapper/models/utils.py
+++ ZoopAPIWrapper/models/utils.py
@@ -3,6 +3,23 @@ from ZoopAPIWrapper.models.bank_account import BankAccount
 
 
 def _get_model_class_from_resource(resource):
+    """
+    getter for model from resource
+
+    Examples:
+        >>> _get_model_class_from_resource('seller')
+        Seller
+        >>> _get_model_class_from_resource('bank_account')
+        BankAccount
+
+    Args:
+        resource: string of resource
+
+    Raises:
+        ValueError: when the resource is not identified
+
+    Returns: ZoopModel subclass
+    """
     if resource == Seller.RESOURCE:
         return Seller
     elif resource == BankAccount.RESOURCE:
@@ -12,6 +29,23 @@ def _get_model_class_from_resource(resource):
 
 
 def get_instance_from_data(data):
+    """
+    getter for model from resource.
+    Factory Pattern
+
+    Examples:
+        >>> data = {'resource': 'seller'}
+        >>> get_instance_from_data(data)
+        Seller.from_dict(data)
+        >>> data = {'resource': 'bank_account'}
+        >>> get_instance_from_data(data)
+        BankAccount.from_dict(data)
+
+    Args:
+        data: dict of data
+
+    Returns: ZoopModel subclass instance or None
+    """
     resource = data.get('resource')
 
     try:

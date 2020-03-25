e739b3b27409d847d19d270b11dea0f18220790a
rodrigondec <rodrigondec@gmail.com>
2020-03-25 12:57:24 -0300

    __init__ imports

---
+++ ZoopAPIWrapper/models/__init__.py
@@ -0,0 +1,3 @@
+from ZoopAPIWrapper.models.bank_account import BankAccount  # noqa
+from ZoopAPIWrapper.models.seller import Seller, IndividualSeller, BusinessSeller  # noqa
+from ZoopAPIWrapper.models.token import Token  # noqa

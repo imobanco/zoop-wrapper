f10abe50ad4218ef5ce9585f7239ae60637f2e25
rodrigondec <rodrigondec@gmail.com>
2020-03-25 17:42:59 -0300

    test bank accounts methods

---
+++ tests/zoop_wrapper/test_bank_accounts_methods.py
@@ -0,0 +1,42 @@
+import json
+from unittest import TestCase
+
+from ZoopAPIWrapper.wrapper import ZoopWrapper
+from ZoopAPIWrapper.models.bank_account import BankAccount
+
+
+class ZoopWrapperBankAccountsMethodsTestCase(TestCase):
+    def setUp(self):
+        self.client = ZoopWrapper()
+
+    def tearDown(self):
+        del self.client
+
+    def test_list_bank_accounts(self):
+        """
+        Test list_bank_accounts method.
+        And create a dump with all bank_accounts.
+        """
+        response = self.client.list_bank_accounts()
+        self.assertEqual(response.status_code, 200, msg=response.data)
+        items = response.data.get('items')
+        self.assertTrue(items)
+        json_object = json.dumps(items, indent=4)
+        with open("data/bank_accounts.json", "w") as outfile:
+            outfile.write(json_object)
+
+    def test_retrieve_bank_account(self):
+        """
+        Test retrieve_bank_account method.
+        Got this bank_account id from the json dump of bank_accounts.
+        """
+        response = self.client.retrieve_bank_account(
+            '064d3c7846b142e591896d2fb69dac3f')
+        self.assertEqual(response.status_code, 200, msg=response.data)
+        data = response.data
+        self.assertEqual(data.get('id'), '064d3c7846b142e591896d2fb69dac3f',
+                         msg=data)
+        self.assertIsInstance(response.instance, BankAccount)
+        self.assertEqual(response.instance.id,
+                         '064d3c7846b142e591896d2fb69dac3f')
+

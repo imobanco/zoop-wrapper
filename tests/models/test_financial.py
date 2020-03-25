d30d296175963a3943021129e9e373497d1b1fab
rodrigondec <rodrigondec@gmail.com>
2020-03-25 16:21:31 -0300

    FinancialTest created

---
+++ tests/models/test_financial.py
@@ -0,0 +1,35 @@
+from unittest import TestCase
+
+from ZoopAPIWrapper.models.base import FinancialModel
+
+
+class FinancialTestCase(TestCase):
+    @property
+    def data(self):
+        return {
+            "status": 'foo',
+            "account_balance": 'foo',
+            "current_balance": 'foo',
+            "description": 'foo',
+            "delinquent": 'foo',
+            "payment_methods": 'foo',
+            "default_debit": 'foo',
+            "default_credit": 'foo',
+        }
+
+    def test_from_dict(self):
+        instance = FinancialModel.from_dict(self.data)
+
+        self.assertIsInstance(instance, FinancialModel)
+        self.assertEqual(instance.status, 'foo')
+        self.assertEqual(instance.account_balance, 'foo')
+        self.assertEqual(instance.current_balance, 'foo')
+        self.assertEqual(instance.description, 'foo')
+        self.assertEqual(instance.delinquent, 'foo')
+        self.assertEqual(instance.default_credit, 'foo')
+        self.assertEqual(instance.default_debit, 'foo')
+
+    def test_to_dict(self):
+        instance = FinancialModel.from_dict(self.data)
+
+        self.assertEqual(instance.to_dict(), self.data)

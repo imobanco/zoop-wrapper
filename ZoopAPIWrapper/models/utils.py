7f7c241fb2f32f5524f27964c432d3a056cbbb72
rodrigondec <rodrigondec@gmail.com>
2020-03-25 11:17:25 -0300

    utils refactored

--- ZoopAPIWrapper/models/utils.py
+++ ZoopAPIWrapper/models/utils.py
@@ -3,15 +3,15 @@ from ZoopAPIWrapper.models.bank_account import BankAccount
 from ZoopAPIWrapper.models.token import Token
 
 
+RESOURCE_CLASSES = [BankAccount, Seller, Token]
+RESOURCES_DICT = {CLASS.RESOURCE: CLASS for CLASS in RESOURCE_CLASSES}
+
+
 def _get_model_class_from_resource(resource):
-    if resource == Seller.RESOURCE:
-        return Seller
-    elif resource == BankAccount.RESOURCE:
-        return BankAccount
-    elif resource == Token.RESOURCE:
-        return Token
-    else:
-        raise ValueError(f'model não identificado para resource {resource}!')
+    if resource in RESOURCES_DICT:
+        return RESOURCES_DICT.get(resource)
+
+    raise ValueError(f'model não identificado para resource {resource}!')
 
 
 def get_instance_from_data(data):

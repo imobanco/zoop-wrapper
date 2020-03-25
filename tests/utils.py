bdf6f9a554ede10ebaa6ff1c5f000f40c1d66f04
rodrigondec <rodrigondec@gmail.com>
2020-03-25 19:55:16 -0300

    patch get

--- tests/utils.py
+++ tests/utils.py
@@ -9,22 +9,31 @@ class BuildResponseMockMixin:
             status_code=status_code,
             json=MagicMock(
                 return_value=content if content else {}
-            )
+            ),
+            instance=None
         )
 
 
-class MockedPostDeleteTestCase(TestCase, BuildResponseMockMixin):
+class RequestsMockedTestCase(TestCase, BuildResponseMockMixin):
     def setUp(self):
         super().setUp()
 
+        self.patcher_get = patch("ZoopAPIWrapper.wrapper.requests.get")
         self.patcher_post = patch("ZoopAPIWrapper.wrapper.requests.post")
         self.patcher_delete = patch("ZoopAPIWrapper.wrapper.requests.delete")
 
+        self.mocked_get = self.patcher_get.start()
         self.mocked_post = self.patcher_post.start()
         self.mocked_delete = self.patcher_delete.start()
 
+        self.addCleanup(self.patcher_get.stop)
         self.addCleanup(self.patcher_post.stop)
         self.addCleanup(self.patcher_delete.stop)
+
+    def set_get_mock(self, status_code=200, content=None):
+        self.mocked_get.return_value = self.build_response_mock(
+            status_code=status_code, content=content
+        )
 
     def set_post_mock(self, status_code=200, content=None):
         self.mocked_post.return_value = self.build_response_mock(

51831d64087f44b10c864cf0555a76117ab5e449
rodrigondec <rodrigondec@gmail.com>
2020-03-25 18:24:39 -0300

    add & remove buyer tests

--- tests/zoop_wrapper/test_buyers_methods.py
+++ tests/zoop_wrapper/test_buyers_methods.py
@@ -57,9 +57,52 @@ class ZoopWrapperBuyerMethodsTestCase(TestCase):
         self.assertEqual(response.instance.id,
                          'd8a5299cc72b4971b42845ed68cbf75c')
 
-    def test_add_buyer(self):
-        pass
+    @patch('ZoopAPIWrapper.wrapper.requests.post')
+    def test_add_buyer(self, mocked_post):
+        mocked_post.return_value = MagicMock(
+            status_code=201,
+            json=MagicMock(
+                return_value={"id": "bf589dd424e64cc49348bf3192bc0db5", "status": "active", "resource": "buyer", "account_balance": "0.00","current_balance": "0.00", "first_name": "foo", "last_name": "foo", "taxpayer_id": "23859566083","description": None, "email": "foo@bar.com", "phone_number": "foo", "facebook": None, "twitter": None,"address": {"line1": None, "line2": None, "line3": None, "neighborhood": None, "city": None, "state": None,"postal_code": None, "country_code": None}, "delinquent": False, "payment_methods": None,"default_debit": None, "default_credit": None, "default_receipt_delivery_method": None,"uri": "/v1/marketplaces/d77c2258b51d49269191502695f939f4/buyers/bf589dd424e64cc49348bf3192bc0db5","metadata": {}, "created_at": "2020-03-25T20:53:13+00:00", "updated_at": "2020-03-25T20:53:13+00:00"}
+            )
+        )
 
-    def test_remove_buyer(self):
-        pass
+        data = {
+            "first_name": "foo",
+            "last_name": "foo",
+            "email": 'foo@bar.com',
+            "taxpayer_id": gen.cpf(),
+            "phone_number": "foo",
+            "birthdate": 'foo',
+            "address": {
+                "line1": "foo",
+                "line2": "foo",
+                "line3": "foo",
+                # "neighborhood": "foo",
+                "city": "foo",
+                "state": "RN",
+                "postal_code": "foo",
+                "country_code": "BR"
+            }
+        }
 
+        response = self.client.add_buyer(data)
+        self.assertEqual(response.status_code, 201, msg=response.data)
+
+    @patch('ZoopAPIWrapper.wrapper.requests.delete')
+    def test_remove_buyer(self, mocked_delete):
+        """
+        Test remove_buyer method.
+        Got the buyer id from buyers dump.
+
+        Args:
+            mocked_delete: mock of object 'delete' from 'requests'
+                            on file 'ZoopAPIWrapper.wrapper'
+        """
+        mocked_delete.return_value = MagicMock(
+            status_code=200,
+            json=MagicMock(
+                return_value={'id': 'a12c6bf830854b5d9c1d2d36cb1478a0', 'resource': 'buyer', 'deleted': True}
+            )
+        )
+        response = self.client.remove_buyer('a12c6bf830854b5d9c1d2d36cb1478a0')
+        self.assertEqual(response.status_code, 200, msg=response.data)

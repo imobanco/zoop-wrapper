712d671d18a699e742085d9bd6110c61a17066fb
rodrigondec <rodrigondec@gmail.com>
2020-03-26 12:00:22 -0300

    mocks on buyer tests

--- tests/zoop_wrapper/test_buyers_methods.py
+++ tests/zoop_wrapper/test_buyers_methods.py
@@ -3,6 +3,7 @@ from pycpfcnpj import gen
 from tests.utils import RequestsMockedTestCase
 from ZoopAPIWrapper.wrapper import ZoopWrapper
 from ZoopAPIWrapper.models.buyer import Buyer
+from ZoopAPIWrapper.models.factories.buyer import BuyerFactory
 
 
 class ZoopWrapperBuyerMethodsTestCase(RequestsMockedTestCase):
@@ -18,6 +19,12 @@ class ZoopWrapperBuyerMethodsTestCase(RequestsMockedTestCase):
         Test list_buyers method.
         And create a dump with all buyers.
         """
+        self.set_get_mock(
+            200,
+            {
+                'items': [BuyerFactory()]
+            }
+        )
 
         response = self.client.list_buyers()
         self.assertEqual(response.status_code, 200, msg=response.data)
@@ -29,6 +36,13 @@ class ZoopWrapperBuyerMethodsTestCase(RequestsMockedTestCase):
         Test retrieve_buyer method.
         Got this buyer id from the json dump of buyers.
         """
+        self.set_get_mock(
+            200,
+            BuyerFactory(
+                id='093bad87c86649528e0286388a325867'
+            ).to_dict()
+        )
+
         response = self.client.retrieve_buyer(
             '093bad87c86649528e0286388a325867')
         self.assertEqual(response.status_code, 200, msg=response.data)
@@ -43,6 +57,14 @@ class ZoopWrapperBuyerMethodsTestCase(RequestsMockedTestCase):
         Test search_buyer method.
         Got this buyer identifier from the json dump of buyers.
         """
+        self.set_get_mock(
+            200,
+            BuyerFactory(
+                id='d8a5299cc72b4971b42845ed68cbf75c',
+                taxpayer_id='13398287409'
+            ).to_dict()
+        )
+
         response = self.client.search_buyer(
             '13398287409')
         self.assertEqual(response.status_code, 200, msg=response.data)
@@ -55,22 +77,8 @@ class ZoopWrapperBuyerMethodsTestCase(RequestsMockedTestCase):
     def test_add_buyer(self):
         self.set_post_mock(
             201,
-            {"id": "bf589dd424e64cc49348bf3192bc0db5", "status": "active",
-             "resource": "buyer", "account_balance": "0.00",
-             "current_balance": "0.00", "first_name": "foo",
-             "last_name": "foo", "taxpayer_id": "23859566083",
-             "description": None, "email": "foo@bar.com",
-             "phone_number": "foo", "facebook": None, "twitter": None,
-             "address": {"line1": None, "line2": None, "line3": None,
-                         "neighborhood": None, "city": None, "state": None,
-                         "postal_code": None, "country_code": None},
-             "delinquent": False, "payment_methods": None,
-             "default_debit": None, "default_credit": None,
-             "default_receipt_delivery_method": None,
-             "uri": "/v1/marketplaces/d77c2258b51d49269191502695f939f4/buyers"
-                    "/bf589dd424e64cc49348bf3192bc0db5", "metadata": {},
-             "created_at": "2020-03-25T20:53:13+00:00",
-             "updated_at": "2020-03-25T20:53:13+00:00"})
+            BuyerFactory().to_dict()
+        )
 
         data = {
             "first_name": "foo",
@@ -96,8 +104,13 @@ class ZoopWrapperBuyerMethodsTestCase(RequestsMockedTestCase):
 
     def test_remove_buyer(self):
         self.set_delete_mock(
-            200, {'id': 'a12c6bf830854b5d9c1d2d36cb1478a0',
-                  'resource': 'buyer', 'deleted': True})
+            200,
+            {
+                'id': 'a12c6bf830854b5d9c1d2d36cb1478a0',
+                'resource': 'buyer',
+                'deleted': True
+            }
+        )
 
         response = self.client.remove_buyer('a12c6bf830854b5d9c1d2d36cb1478a0')
         self.assertEqual(response.status_code, 200, msg=response.data)

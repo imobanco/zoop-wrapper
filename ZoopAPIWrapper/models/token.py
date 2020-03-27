f6c431c55c3ff4fb49d0a21b981cbec76e53f754
rodrigondec <rodrigondec@gmail.com>
2020-03-27 13:16:14 -0300

    BankAccountToken created

--- ZoopAPIWrapper/models/token.py
+++ ZoopAPIWrapper/models/token.py
@@ -37,6 +37,67 @@ class Token(ZoopModel):
         self.type = type
         self.used = used
 
+class BankAccountToken(BusinessOrIndividualMixin, Token):
+    """
+    Token is a resource used to link a BankAccount and a Customer
+    https://docs.zoop.co/reference#post_v1-marketplaces-marketplace-id-bank-accounts-tokens
+
+    This class and it's subclasses have attributes.
+
+    The __FIELDS list the attributes this class
+    has responsability of constructing in the serialization to dict.
+
+    The RESOURCE attribute of this class is used to identify this Model.
+    Remember the resource on ZoopModel? BAM!
+
+    Attributes:
+        holder_name: name of owner
+        bank_code: number of card
+        routing_number: month of expiration
+        account_number: year of expiration
+        type: security code
+        bank_account: BankAccount model
+    """
+    __FIELDS = ['holder_name', 'card_number', 'expiration_month',
+                'expiration_year', 'security_code', 'card']
+
+    def __init__(self, holder_name, bank_code, routing_number,
+                 account_number, type,
+                 bank_account=None, **kwargs):
+        super().__init__(type=None, used=None, **kwargs)
+
+        self.holder_name = holder_name
+        self.bank_code = bank_code
+        self.routing_number = routing_number
+        self.account_number = account_number
+        self.type = type
+        try:
+            self.bank_account = BankAccount.from_dict(bank_account)
+        except TypeError as e:
+            e.args = (f'{BankAccount} could not be created!',)
+            logger.warning(e)
+            self.bank_account = None
+
+    # noinspection PyMethodParameters
+    @classproperty
+    def business_class(cls):
+        return BusinessBankAccountToken
+    # noinspection PyMethodParameters
+    @classproperty
+    def individual_class(cls):
+        return IndividualBankAccountToken
+
+    @property
+        """
+    def fields(self):
+        the fields of ZoopBase are it's
+        __FIELDS extended with it's father fields.
+        it's important to be a new list (high order function)
+        Returns: new list of attributes
+        """
+        super_fields = super().fields
+        super_fields.extend(self.__FIELDS)
+        return list(super_fields)
 class CardToken(Token):
     """
     Token is a resource used to link a Card and a Customer

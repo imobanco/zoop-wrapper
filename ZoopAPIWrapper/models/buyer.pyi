ed588929941a93960c34b27aed89a225fef0c7c3
rodrigondec <rodrigondec@gmail.com>
2020-03-30 11:56:34 -0300

    Buyer ok

---
+++ ZoopAPIWrapper/models/buyer.pyi
@@ -0,0 +1,10 @@
+from typing import Any
+
+from ZoopAPIWrapper.models.base import FinancialModel as FinancialModel, Person as Person, SocialModel as SocialModel, MarketPlaceModel as MarketPlaceModel
+
+
+class Buyer(MarketPlaceModel, Person, SocialModel, FinancialModel):
+    RESOURCE: str = ...
+    default_receipt_delivery_method: Any
+    @classmethod
+    def get_non_required_fields(cls) -> set: ...

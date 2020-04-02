481cea24549fca415d3b750e3a1dd483c09f4f3b
rodrigondec <rodrigondec@gmail.com>
2020-04-02 11:08:08 -0300

    test card verification

--- tests/models/test_card_verficiation_check.py
+++ tests/models/test_card_verficiation_check.py
@@ -1,34 +1,19 @@
-from unittest import TestCase
+from unittest.mock import MagicMock
 
+from tests.utils import SetTestCase
 from ZoopAPIWrapper.models.card import CardVerificationChecklist
 from ZoopAPIWrapper.models.factories.card import (
     CardVerificationChecklistFactory
 )
 
 
-class CardVerificationChecklistTestCase(TestCase):
-    @property
-    def data(self):
-        return {
-            "postal_code_check": 'foo',
-            "address_line1_check": 'foo',
-
-            "security_code_check": 'foo'
-        }
+class CardVerificationChecklistTestCase(SetTestCase):
+    def test_get_required_fields(self):
+        self.assertIsSubSet(
+            {'security_code_check'},
+            CardVerificationChecklist.get_required_fields()
+        )
 
     def test_create(self):
         instance = CardVerificationChecklistFactory()
         self.assertIsInstance(instance, CardVerificationChecklist)
-
-    def test_from_dict(self):
-        instance = CardVerificationChecklist.from_dict(self.data)
-
-        self.assertIsInstance(instance, CardVerificationChecklist)
-        self.assertEqual(instance.postal_code_check, 'foo')
-        self.assertEqual(instance.address_line1_check, 'foo')
-        self.assertEqual(instance.security_code_check, 'foo')
-
-    def test_to_dict(self):
-        instance = CardVerificationChecklist.from_dict(self.data)
-
-        self.assertEqual(instance.to_dict(), self.data)

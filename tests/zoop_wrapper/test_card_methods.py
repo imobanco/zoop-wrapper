57d53e3ba5ded7a14ad25ead9548ca6764145017
rodrigondec <rodrigondec@gmail.com>
2020-04-03 12:12:44 -0300

    card methods

--- tests/zoop_wrapper/test_card_methods.py
+++ tests/zoop_wrapper/test_card_methods.py
@@ -1,8 +1,13 @@
+from unittest.mock import patch, MagicMock
+
 from tests.utils import RequestsMockedTestCase
 from ZoopAPIWrapper.wrapper import ZoopWrapper
 from ZoopAPIWrapper.models.card import Card
-from tests.factories.card import (
-    CardFactory)
+from tests.factories.buyer import BuyerFactory
+from tests.factories.card import CardFactory
+from tests.factories.token import (
+    CardTokenFactory, CreateCardTokenFactory
+)
 
 
 class ZoopWrapperCardMethodsTestCase(RequestsMockedTestCase):
@@ -30,53 +35,47 @@ class ZoopWrapperCardMethodsTestCase(RequestsMockedTestCase):
         self.assertIsInstance(response.instance, Card)
         self.assertEqual(response.instance.id, 'foo')
 
-    # def test_add_card_token(self):
-    #     """
-    #     Test __add_card_token method.
-    #     """
-    #     self.set_post_mock(
-    #         201,
-    #         TokenFactory(
-    #             type='card'
-    #         ).to_dict()
-    #     )
-    #
-    #     card = CardFactory()
-    #
-    #     response = self.client.\
-    #         _ZoopWrapper__add_card_token(card)
-    #     self.assertEqual(response.status_code, 201, msg=response.data)
-    #
-    # @patch('ZoopAPIWrapper.wrapper.ZoopWrapper._ZoopWrapper__add_card_token')
-    # def test_add_card(self, mocked_add_token):
-    #     """
-    #     Test add_card method.
-    #     """
-    #     mocked_add_token.return_value = MagicMock(
-    #         instance=TokenFactory(
-    #             type='card'
-    #         )
-    #     )
-    #
-    #     data = CardFactory(
-    #         id='foo',
-    #         customer='bar'
-    #     ).to_dict()
-    #
-    #     self.set_post_mock(
-    #         200,
-    #         data
-    #     )
-    #
-    #     self.set_get_mock(
-    #         200,
-    #         IndividualSellerFactory(
-    #             id='bar'
-    #         ).to_dict()
-    #     )
-    #
-    #     response = self.client.add_card(data)
-    #     self.assertEqual(response.status_code, 200, msg=response.data)
-    #     self.assertEqual(data.get('id'), 'foo', msg=data)
-    #     self.assertIsInstance(response.instance, Card)
-    #     self.assertEqual(response.instance.id, 'foo')
+    def test_add_card_token(self):
+        """
+        Test __add_card_token method.
+        """
+        self.set_post_mock(
+            201,
+            CardTokenFactory().to_dict()
+        )
+
+        instance = CreateCardTokenFactory()
+
+        response = self.client.\
+            _ZoopWrapper__add_card_token(instance)
+        self.assertEqual(response.status_code, 201, msg=response.data)
+
+    @patch('ZoopAPIWrapper.wrapper.ZoopWrapper._ZoopWrapper__add_card_token')
+    def test_add_card(self, mocked_add_token):
+        """
+        Test add_card method.
+        """
+        mocked_add_token.return_value = MagicMock(
+            instance=CardTokenFactory()
+        )
+
+        self.set_post_mock(
+            200,
+            CardFactory(
+                id='foo'
+            ).to_dict()
+        )
+
+        self.set_get_mock(
+            200,
+            BuyerFactory(
+                id='bar'
+            ).to_dict()
+        )
+
+        data = CreateCardTokenFactory().to_dict()
+
+        response = self.client.add_card(data, 'bar')
+        self.assertEqual(response.status_code, 200, msg=response.data)
+        self.assertIsInstance(response.instance, Card)
+        self.assertEqual(response.instance.id, 'foo')

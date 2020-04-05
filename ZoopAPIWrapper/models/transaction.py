5db32480bfbb11caa8df6b6c2d081b6fc7e097bb
rodrigondec <rodrigondec@gmail.com>
2020-04-05 16:31:55 -0300

    Transaction init_custom_fields & tests

--- ZoopAPIWrapper/models/transaction.py
+++ ZoopAPIWrapper/models/transaction.py
@@ -33,6 +33,29 @@ class Transaction(ResourceModel):
     """
     RESOURCE = 'transaction'
 
+    def init_custom_fields(self, payment_method=None,
+                           point_of_sale=None, history=None,
+                           **kwargs):
+        setattr(self, 'payment_method', payment_method)
+
+        setattr(
+            self, 'point_of_sale',
+            PointOfSale.from_dict_or_instance(
+                point_of_sale, allow_empty=True))
+
+        if isinstance(history, list):
+            setattr(
+                self, 'history',
+                [
+                    History.from_dict_or_instance(item, allow_empty=True)
+                    for item in history
+                ])
+        else:
+            setattr(
+                self, 'history',
+                [History.from_dict_or_instance(history, allow_empty=True)]
+            )
+
     @classmethod
     def get_required_fields(cls):
         fields = super().get_required_fields()
---
+++ tests/models/trasaction/test_transaction.py
@@ -0,0 +1,27 @@
+from tests.utils import SetTestCase
+from ZoopAPIWrapper.models.transaction import Transaction
+
+
+class TransactionTestCase(SetTestCase):
+    def test_non_required_fields(self):
+        self.assertIsSubSet(
+            {"status", "confirmed", "original_amount", "transaction_number",
+             "gateway_authorizer", "app_transaction_uid", "refunds", "rewards",
+             "discounts", "pre_authorization", "sales_receipt",
+             "statement_descriptor", "point_of_sale", "installment_plan",
+             "refunded", "voided", "captured", "fees", "fee_details",
+             "location_latitude", "location_longitude", "individual",
+             "business", "expected_on", "history"},
+            Transaction.get_non_required_fields()
+        )
+
+    def test_required_fields(self):
+        self.assertEqual(
+            {'amount', 'currency', 'description', 'reference_id',
+             'on_behalf_of', 'customer', 'payment_type', 'payment_method'},
+            Transaction.get_required_fields()
+        )
+
+    def test_to_dict_empty(self):
+        instance = Transaction(allow_empty=True)
+        self.assertEqual(instance.to_dict(), {})

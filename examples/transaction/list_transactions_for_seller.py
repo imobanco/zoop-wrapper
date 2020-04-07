8b478eccf98166bd1fa011b45e275f013112c23f
rodrigondec <rodrigondec@gmail.com>
2020-04-07 14:22:37 -0300

    removed marketplace_id from data

--- examples/transaction/list_transactions_for_seller.py
+++ examples/transaction/list_transactions_for_seller.py
@@ -8,4 +8,4 @@ client = ZoopWrapper()
 response = client.list_transactions_for_seller('27e17b778b404a83bf8e25ec995e2ffe')
 
 with open('./data/transactions_seller.json', 'w') as file:
-    json.dump(response.data.get('items'), file, indent=4)
+    json.dump(response.data, file, indent=4)

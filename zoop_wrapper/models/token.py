278613c1f496f3147d6975701d70017cb7d08706
rodrigondec <rodrigondec@gmail.com>
2020-04-08 17:24:48 -0300

    token docstrings

--- zoop_wrapper/models/token.py
+++ zoop_wrapper/models/token.py
@@ -2,6 +2,7 @@ from zoop_wrapper.models.base import ResourceModel, BusinessOrIndividualModel
 from zoop_wrapper.models.bank_account import BankAccount
 from zoop_wrapper.models.card import Card
 from zoop_wrapper.utils import get_logger
+from zoop_wrapper.exceptions import FieldError, ValidationError
 
 
 logger = get_logger('models')
@@ -9,65 +10,51 @@ logger = get_logger('models')
 
 class Token(ResourceModel):
     """
-    Token is a resource used to link a BankAccount Or Card and a Customer
+    Token is a resource used to link a :class:`.BankAccount` Or
+    :class:`.Card` and a :class:`.Seller` or :class:`.Buyer`.
     https://docs.zoop.co/reference#token-1
 
-    The RESOURCE attribute of this class is used to identify this Model.
-    Remember the resource on ZoopModel? BAM!
+    The :attr:`RESOURCE` is used to identify this Model.
+    Used to check against :attr:`.resource`!
 
-    This has dynamic types!
-    It can be 'card' or 'bank_account'.
-    But on creation it doesn't have attribute type.
-    So we need to verify by other attributes.
+    It has ``dynamic types``!
 
-    After created it will have 'type'.
-    So we need a boolean to control if we get type by other attribute or
-    from 'type'.
+    It can be :attr:`CARD_TYPE` or :attr:`BANK_ACCOUNT_TYPE`.
+
+    But before ``creation`` it won't have attribute ``type``.
+    So we need to verify by ``other attributes``.
+    After ``created`` on Zoop it will have ``type``.
 
     Attributes:
-        RESOURCE = 'token'
+        token_type (str): value for identified token ``type``
 
-        CARD_TYPE: str 'card' for card type
-        CARD_IDENTIFIER: card attribute ('card_number') used to identify type
+        type (str): optional :attr:`BANK_ACCOUNT_TYPE` or :attr:`CARD_TYPE`.
+            It has collision with of :attr:`.BankAccount.type`. So we need the above
+            :attr:`token_type`.
+        used (bool): optional value of verification
 
-        BANK_ACCOUNT_TYPE: str 'bank_account' for bank account type
-        BANK_ACCOUNT_IDENTIFIER: bank account attribute ('bank_code')
-            used to identify type
-
-        TYPES: set of types {CARD_TYPE, BANK_ACCOUNT_TYPE}
-        IDENTIFIERS: set of identifiers {CARD_IDENTIFIER,
-                                        BANK_ACCOUNT_IDENTIFIER}
-
-        token_type: str for identified token type set by TYPE_ATTR
-
-        type: optional bank_account or card. It has collision with
-            BankAccount.type. So we need the above token_type
-        used: optional boolean of verification
-
-        bank_account: optional BankAccount instance model
-            (for created token of 'bank_account' type)
-        card: optional Card instance model (for created token of
+        bank_account (:class:`.BankAccount`): optional value
+            (for ``created`` token of 'bank_account' type)
+        card (:class:`.Card`): optional value (for ``created`` token of
             'card' type)
 
-        holder_name: owner name (for both token of 'bank_account'
+        holder_name (str): owner name (for both token of 'bank_account'
             and 'card' type)
 
-        account_number: account number of bank account (for token of
-            'bank_account' type)
-        taxpayer_id: if is for individual bank account (for token of
-            'bank_account' type)
-        ein: if is for business bank account (for token of 'bank_account' type)
-        bank_code: bank code (for token of 'bank_account' type)
-        routing_number: agency code in BR (for token of 'bank_account' type)
+        account_number (str): account number for :attr:`BANK_ACCOUNT_TYPE`
+        taxpayer_id (str): identifier for :attr:`BANK_ACCOUNT_TYPE` of
+            :attr:`.INDIVIDUAL_TYPE`
+        ein (str): identifier for :attr:`BANK_ACCOUNT_TYPE` of
+            :attr:`.BUSINESS_TYPE`
+        bank_code (str): bank code for :attr:`BANK_ACCOUNT_TYPE`
+        routing_number (str): agency code in BR for :attr:`BANK_ACCOUNT_TYPE`
 
-        expiration_month: month of expiration (for token of 'card' type)
-        expiration_year: year of expiration (for token of 'card' type)
-        card_number: card number (for token of 'card' type)
-        security_code: security code (for token of 'card' type)
+        expiration_month (str): month of expiration for :attr:`CARD_TYPE`
+        expiration_year (str): year of expiration for :attr:`CARD_TYPE`
+        card_number (str): card number for :attr:`CARD_TYPE`
+        security_code (str): security code for :attr:`CARD_TYPE`
     """
     RESOURCE = 'token'
-
-    TYPE_ATTR = 'token_type'
 
     CARD_TYPE = 'card'
     CARD_IDENTIFIER = 'card_number'
@@ -81,20 +68,21 @@ class Token(ResourceModel):
     def init_custom_fields(self, type=None, card=None,
                            bank_account=None, **kwargs):
         """
-        if type is 'bank_account' or 'card' token is created!
-        set card or bank_account attributes accordingly.
+        if ``type`` is :attr:`BANK_ACCOUNT_TYPE` or :attr:`CARD_TYPE`
+        token is ``created``!\n
+        set :attr:`card` or :attr:`bank_account` attributes accordingly.
 
-        else token is not created!
-        we must identify token type from attr's passed
-        searching for CARD_IDENTIFIER or BANK_ACCOUNT_IDENTIFIER.
-        After identifying type if it was 'bank_account' set business
-        or individual identifier from BankAccount method (which is
-        from BusinessOrIndividualModel).
+        else token is ``not created``!\n
+        We must identify token type from attr's passed
+        searching for :attr:`CARD_IDENTIFIER` or :attr:`BANK_ACCOUNT_IDENTIFIER`.
+        After identifying ``type`` if it was :attr:`BANK_ACCOUNT_TYPE` set ``business``
+        or ``individual`` identifier from :class:`.BankAccount` method (which is
+        from :class:`.BusinessOrIndividualModel`).
 
         Args:
-            type: type for token or bank account
-            card: Card instance model or data
-            bank_account: BankAccount instance model or data
+            type (str): type for ``token`` or ``bank account``
+            card (dict or :class:`.Card`): data
+            bank_account (dict or :class:`.BankAccount`):  data
             **kwargs: kwargs
         """
         if type in self.TYPES:
@@ -118,36 +106,24 @@ class Token(ResourceModel):
                 token_type = self.BANK_ACCOUNT_TYPE
                 BusinessOrIndividualModel.set_identifier(self, **kwargs)
             else:
-                raise TypeError(
-                    f'Token type not identified! '
-                    f'Please set one of these attributes {self.IDENTIFIERS}')
+                raise ValidationError(
+                    self, FieldError(
+                        'token_type',
+                        f'Token type not identified! '
+                        f'Please set one of these attributes {self.IDENTIFIERS}')
+                )
 
         setattr(self, 'token_type', token_type)
 
-    def get_type(self):
-        """
-        get token type value
-
-        Raises:
-            TypeError: when token type is not set!
-
-        Returns: str with token type
-        """
-        try:
-            return self.token_type
-        except AttributeError as e:
-            raise TypeError(
-                f'Token type not identified! '
-                f'Please set one of these attributes {self.IDENTIFIERS}') from e
-
     def get_bank_account_type(self):
         """
-        get bank account type for creation token of BankAccount
+        Get ``bank account type`` for ``creation token`` of :class:`.BankAccount.
 
         Raises:
             TypeError: when called  from a token not from 'bank_account' type
 
-        Returns: str with bank_account type
+        Returns:
+            value with bank_account type
         """
         if self.token_type == self.BANK_ACCOUNT_TYPE:
             try:
@@ -158,21 +134,25 @@ class Token(ResourceModel):
 
     def get_validation_fields(self):
         """
-        get validation fields for types.
+        Get ``validation fields`` for instance.\n
 
-        if token is created return bare required_fields.
+        if :attr:`token_type` is :attr:`CARD_TYPE` card return
+        :meth:`get_card_required_fields`.
 
-        elif token type is card return card_required_fields.
+        else :attr:`token_type` is :attr:`BANK_ACCOUNT_TYPE`!
+        ``fields`` is :meth:`get_bank_account_required_fields`.\n
+        if ``bank account type`` is :attr:`.INDIVIDUAL_TYPE` return ``fields`` union
+        :meth:`.get_individual_required_fields`.\n
 
-        else token type is bank account:
-        if bank account is for individual
+        else ``bank account type`` is :attr:`.BUSINESS_TYPE` return ``fields`` union
+        :meth:`.get_business_required_fields`.
 
-        Returns: set of fields to be validated
+        Returns:
+            ``set`` of fields to be validated
         """
-        fields = self.get_required_fields()
-        token_type = self.get_type()
+        fields = set()
 
-        if token_type == self.CARD_TYPE:
+        if self.token_type == self.CARD_TYPE:
             return fields.union(
                 self.get_card_required_fields()
             )
@@ -191,21 +171,22 @@ class Token(ResourceModel):
 
     def get_all_fields(self):
         """
-        get all fields for types.
+        Get ``all fields`` for instance.
 
-        construct set 'fields' from get_validation_fields.
+        ``fields`` is :meth:`get_validation_fields`
 
-        if token type is card return fields union get_card_non_required_fields.
+        if :attr:`token_type` is :attr:`CARD_TYPE` return
+        ``fields`` union :meth:`get_card_non_required_fields`.
 
-        else token type is bank account return fields union
-        get_bank_account_non_required_fields.
+        else :attr:`token_type` is :attr:`BANK_ACCOUNT_TYPE` return
+        ``fields`` union :meth:`get_bank_account_non_required_fields`.
 
-        Returns: set of all fields
+        Returns:
+            ``set`` of all fields
         """
         fields = self.get_validation_fields()
 
-        token_type = self.get_type()
-        if token_type == self.CARD_TYPE:
+        if self.token_type == self.CARD_TYPE:
             return fields.union(
                 self.get_card_non_required_fields()
             )
@@ -216,11 +197,6 @@ class Token(ResourceModel):
 
     @classmethod
     def get_non_required_fields(cls):
-        """
-        get set of non required fields
-
-        Returns: set of fields
-        """
         fields = super().get_non_required_fields()
         return fields.union(
             {'type', 'used'}
@@ -229,9 +205,10 @@ class Token(ResourceModel):
     @classmethod
     def get_card_non_required_fields(cls):
         """
-        get set of non required fields for 'card' type
+        Get ``set`` of ``non required fields`` for :attr:`CARD_TYPE`.
 
-        Returns: set of fields
+        Returns:
+            ``set`` of fields
         """
         fields = cls.get_non_required_fields()
         return fields.union(
@@ -241,9 +218,10 @@ class Token(ResourceModel):
     @classmethod
     def get_card_required_fields(cls):
         """
-        get set of required fields for 'card' type
+        Get ``set`` of ``required fields`` for :attr:`CARD_TYPE`.
 
-        Returns: set of fields
+        Returns:
+            ``set`` of fields
         """
         fields = cls.get_required_fields()
         return fields.union(
@@ -254,9 +232,10 @@ class Token(ResourceModel):
     @classmethod
     def get_bank_account_non_required_fields(cls):
         """
-        get set of non required fields for 'bank_account' type
+        Get ``set`` of ``non required fields`` for :attr:`BANK_ACCOUNT_TYPE`
 
-        Returns: set of fields
+        Returns:
+            ``set`` of fields
         """
         fields = cls.get_non_required_fields()
         return fields.union(
@@ -266,9 +245,10 @@ class Token(ResourceModel):
     @classmethod
     def get_bank_account_required_fields(cls):
         """
-        get set of required fields for 'bank_account' type
+        get ``set`` of ``required fields`` for :attr:`BANK_ACCOUNT_TYPE`
 
-        Returns: set of fields
+        Returns:
+            ``set`` of fields
         """
         fields = cls.get_required_fields()
         return fields.union(

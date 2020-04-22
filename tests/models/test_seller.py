19738ef5c3edc56b23fc9978e235bdd9c2723a6b
rodrigondec <rodrigondec@gmail.com>
2020-04-21 22:56:18 -0300

    fix import

--- tests/models/test_seller.py
+++ tests/models/test_seller.py
@@ -1,7 +1,5 @@
 from unittest.mock import patch, MagicMock
 
-from pycpfcnpj import gen
-
 from tests.utils import MockedLoggerTestCase as TestCase, SetTestCase
 from zoop_wrapper.models.seller import Seller
 from zoop_wrapper.exceptions import ValidationError

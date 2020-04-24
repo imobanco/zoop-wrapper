4759e855f297ccc6dc830498d7fa8da8a3a976f5
rodrigondec <rodrigondec@gmail.com>
2020-04-24 11:57:44 -0300

    relative imports

--- zoop_wrapper/response.pyi
+++ zoop_wrapper/response.pyi
@@ -4,6 +4,7 @@ from requests.models import Response
 
 from zoop_wrapper.models.base import ResourceModel
 
+
 class ZoopResponse(Response):
     data = Dict[str, Any]
     instance = Optional[ResourceModel]

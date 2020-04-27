7bc56c122529f04a8a092eb68e533ad16ad4c230
rodrigondec <rodrigondec@gmail.com>
2020-04-27 14:42:40 -0300

    removed garbage comments & path

--- docs/conf.py
+++ docs/conf.py
@@ -12,11 +12,9 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-# sys.path.insert(0, os.path.abspath('.'))
 
-# Insert Requests' path into the system.
+# Insert zoop-wrapper path into the system.
 sys.path.insert(0, os.path.abspath(".."))
-sys.path.insert(0, os.path.abspath("_themes"))
 
 import zoop_wrapper
 

aba371f2e714840018ba968b01864a25cacd7756
rodrigondec <rodrigondec@gmail.com>
2020-04-27 17:07:14 -0300

    search methods example

---
+++ examples/seller/search_individual_seller.py
@@ -0,0 +1,17 @@
+import os
+
+from zoop_wrapper import ZoopWrapper
+from examples.utils import dump_response
+
+"""
+Nesse momento as constantes podem ser criadas no arquivo .py.
+Mas é recomendado utilizar como variável de ambiente em um '.env'
+"""
+from zoop_wrapper.constants import MARKETPLACE_ID, ZOOP_KEY
+
+
+client = ZoopWrapper(marketplace_id=MARKETPLACE_ID, key=ZOOP_KEY)
+
+response = client.search_individual_seller('13543402480')
+
+dump_response(response, os.path.basename(__file__).split(".")[0])

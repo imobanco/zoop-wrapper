46e9c6252befbdfd7f71d014226a373da8d88189
rodrigondec <rodrigondec@gmail.com>
2020-04-27 16:50:50 -0300

    remove example

---
+++ examples/seller/remove_seller.py
@@ -0,0 +1,17 @@
+import os
+
+from zoop_wrapper import ZoopWrapper
+from examples.utils import dump_response
+
+"""
+Nesse momento as constantes podem ser criadas no arquivo .py.
+Mas é recomendado utilizar como variável de ambiente em um '.env'
+"""
+from zoop_wrapper.constants import MARKETPLACE_ID, ZOOP_KEY
+
+
+client = ZoopWrapper(marketplace_id=MARKETPLACE_ID, key=ZOOP_KEY)
+
+response = client.remove_seller("79bb6be6ac8a477cbc1d8f79236a0f75")
+
+dump_response(response, os.path.basename(__file__).split(".")[0])

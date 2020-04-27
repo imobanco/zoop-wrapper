1356cdf1182ea5a2d8632f44752ff526cc737cd9
rodrigondec <rodrigondec@gmail.com>
2020-04-27 12:31:40 -0300

    better warning description

--- zoop_wrapper/response.py
+++ zoop_wrapper/response.py
@@ -5,7 +5,7 @@ class ZoopResponse(Response):
     """
     Uma :class:`requests.Response` recebida da API da Zoop.
 
-    .. danger:: Essa classe NÃO existe de verdade.
+    .. danger:: Essa classe NÃO é utilizada no código (não é instanciada).
 
         Está na biblioteca apenas para ter o type hinting.
         Esses atributos são adicionados ao objeto :class:`requests.Response`

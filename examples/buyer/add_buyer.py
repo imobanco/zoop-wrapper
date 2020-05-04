6edcd4c706677c561a3f117b1602b1a43cd46d2a
rodrigondec <rodrigondec@gmail.com>
2020-05-04 16:46:30 -0300

    add, list & retrieve ok

---
+++ examples/buyer/add_buyer.py
@@ -0,0 +1,39 @@
+import os
+
+from pycpfcnpj import gen
+
+from zoop_wrapper import ZoopWrapper, Buyer, Address
+from examples.utils import dump_response
+
+"""
+Nesse momento as constantes podem ser criadas no arquivo .py.
+Mas é recomendado utilizar como variável de ambiente em um '.env'
+"""
+from zoop_wrapper.constants import MARKETPLACE_ID, ZOOP_KEY
+
+
+client = ZoopWrapper(marketplace_id=MARKETPLACE_ID, key=ZOOP_KEY)
+
+b = Buyer(
+    taxpayer_id=gen.cpf(),
+    first_name="foo",
+    last_name="foo",
+    email="foo@bar.com",
+    phone_number="+55 84 99999-9999",
+    birthdate="1994-12-27",
+    address=Address(
+        line1="foo",
+        line2="123",
+        line3="barbar",
+        neighborhood="fooofoo",
+        city="Natal",
+        state="RN",
+        postal_code="59152250",
+        country_code="BR",
+    ),
+)
+
+
+response = client.add_buyer(b)
+
+dump_response(response, os.path.basename(__file__).split(".")[0])

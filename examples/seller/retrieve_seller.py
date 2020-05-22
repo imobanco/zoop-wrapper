95e27cf5e77a4eed94ddb1e76c2c15a8ef73b274
rodrigondec <rodrigondec@gmail.com>
2020-05-22 12:35:25 -0300

    corrige pequeno problema

--- examples/seller/retrieve_seller.py
+++ examples/seller/retrieve_seller.py
@@ -14,6 +14,6 @@ client = ZoopWrapper(marketplace_id=MARKETPLACE_ID, key=ZOOP_KEY)
 
 seller_id = "0e084bb6a60f47e8ac45949d5040eb92"
 
-response = client.retrieve_seller()
+response = client.retrieve_seller(seller_id)
 
 dump_response(response, os.path.basename(__file__).split(".")[0])

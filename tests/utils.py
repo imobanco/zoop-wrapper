5943b73043646875ba670f09e57b2a00c3bf9c1d
rodrigondec <rodrigondec@gmail.com>
2020-06-29 13:46:02 -0300

    adiciona infos para auxiliar nos testes do wrapper

--- tests/utils.py
+++ tests/utils.py
@@ -28,10 +28,10 @@ class APITestCase(TestCase):
         self.patcher_post = patch("zoop_wrapper.wrapper.base.requests.post")
         self.patcher_put = patch("zoop_wrapper.wrapper.base.requests.put")
 
-        self.mocked_delete = self.patcher_delete.start()
-        self.mocked_get = self.patcher_get.start()
-        self.mocked_post = self.patcher_post.start()
-        self.mocked_put = self.patcher_put.start()
+        self.mocked_delete: MagicMock = self.patcher_delete.start()
+        self.mocked_get: MagicMock = self.patcher_get.start()
+        self.mocked_post: MagicMock = self.patcher_post.start()
+        self.mocked_put: MagicMock = self.patcher_put.start()
 
         self.addCleanup(self.patcher_delete.stop)
         self.addCleanup(self.patcher_get.stop)
@@ -39,6 +39,9 @@ class APITestCase(TestCase):
         self.addCleanup(self.patcher_put.stop)
 
         self.client = ZoopWrapper(marketplace_id="foo", key="foo")
+
+        self.base_url = self.client._RequestsWrapper__base_url
+        self.auth = self.client._auth
 
     def tearDown(self):
         super().tearDown()
@@ -58,6 +61,11 @@ class APITestCase(TestCase):
         response.raise_for_status = raise_for_status
         return response
 
+    def set_delete_mock(self, status_code=200, content=None):
+        self.mocked_delete.return_value = self.build_response_mock(
+            status_code=status_code, content=content
+        )
+
     def set_get_mock(self, status_code=200, content=None):
         self.mocked_get.return_value = self.build_response_mock(
             status_code=status_code, content=content
@@ -68,7 +76,8 @@ class APITestCase(TestCase):
             status_code=status_code, content=content
         )
 
-    def set_delete_mock(self, status_code=200, content=None):
-        self.mocked_delete.return_value = self.build_response_mock(
+    def set_put_mock(self, status_code=200, content=None):
+        self.mocked_put.return_value = self.build_response_mock(
             status_code=status_code, content=content
         )
+

bd0da7cd85fd57cabaeb66dfaf64b0bf6067e5a4
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2020-09-10 10:39:35 -0300

    Feature/installmente plan fix (#105)
    * Corrige installment_plan
    
    * Corrige testes

--- tests/models/transaction/test_source.py
+++ tests/models/transaction/test_source.py
@@ -4,6 +4,7 @@ from tests.utils import SetTestCase
 from zoop_wrapper.models.transaction import Source
 from tests.factories.token import CreateCardTokenFactory
 from tests.factories.source import SourceCardPresentFactory, SourceCardNotPresentFactory
+from tests.factories.installment_plan import InstallmentPlanFactory
 
 from zoop_wrapper.models.token import Token
 from zoop_wrapper.exceptions import ValidationError
@@ -23,15 +24,62 @@ class SourceTestCase(SetTestCase):
         )
 
     def test_init_custom_fields_card_present_type(self):
+        """
+        Dado:
+            - Um cartão qualquer válido c1
+            - Um installment_plan qualquer válido ip1
+            - usage="single_use"
+            - amount=1234
+        Quando:
+            - for chamado Source.init_custom_fields( ...)
+        Então:
+            - Nenhum erro deve ocorrer
+            - instance.card deve ser uma instância de Token
+        """
+        c1 = CreateCardTokenFactory()
+        ip1 = InstallmentPlanFactory()
+
         instance = MagicMock()
 
         Source.init_custom_fields(
-            instance, card=CreateCardTokenFactory(), usage="single_use", amount=1234
+            instance,
+            card=c1,
+            usage="single_use",
+            amount=1234,
+            installment_plan=ip1,
         )
 
         self.assertIsInstance(instance.card, Token)
 
+    def test_init_custom_fields_card_present_type_installment_plan_raise(self):
+        """
+        Dado:
+            - Um installment_plan inválido installment_plan_invalid
+        Quando:
+            - for chamado Source.init_custom_fields( ..., installment_plan=installment_plan_invalid) # noqa
+        Então:
+            - O erro ValidationError deve ser lançado
+
+        Note:
+            - O number_installments só pode ser de 1 até 12
+            - O mode só pode ser ou with_interest ou interest_free
+        """
+
+        installment_plan_invalid = MagicMock(mode="foo-bar", number_installments=11234)
+
+        instance = MagicMock()
+
+        with self.assertRaises(ValidationError):
+            Source.init_custom_fields(
+                instance,
+                card=CreateCardTokenFactory(),
+                usage="single_use",
+                amount=1234,
+                installment_plan=installment_plan_invalid,
+            )
+
     def test_required_fields(self):
+
         self.assertEqual(
             {"card", "type", "usage", "currency", "amount", "installment_plan"},
             Source.get_required_fields(),

df60d4a3f427ea2621bdcf38004c323fc57df07d
Rodrigo Castro <rodrigondec@gmail.com>
2020-09-30 10:41:28 -0300

    sobe BDD's (#107)
    * sobe BDD's
    
    * fmt

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,33 +6,83 @@ from zoop_wrapper.exceptions import FieldError
 
 class UtilsTestCase(TestCase):
     def test_convert_currency_float_value_to_cents_int_input(self):
+        """
+        Dado:
+            - um valor 1234
+        Quando:
+            - for chamado convert_currency_float_value_to_cents(value)
+        Então:
+            - o resultado deve ser 1234
+        """
         value = 1234
         expected = 1234
         result = convert_currency_float_value_to_cents(value)
         self.assertEqual(expected, result)
 
     def test_convert_currency_float_value_to_cents_float_input(self):
+        """
+        Dado:
+            - um valor 56.78123123123123123
+        Quando:
+            - for chamado convert_currency_float_value_to_cents(value)
+        Então:
+            - o resultado deve ser 5678
+        """
         value = 56.78123123123123123
         expected = 5678
         result = convert_currency_float_value_to_cents(value)
         self.assertEqual(expected, result)
 
     def test_convert_currency_float_value_to_cents_str_with_int_input(self):
+        """
+        Dado:
+            - um valor "9876"
+        Quando:
+            - for chamado convert_currency_float_value_to_cents(value)
+        Então:
+            - o resultado deve ser 9876
+        """
         value = "9876"
         expected = 9876
         result = convert_currency_float_value_to_cents(value)
         self.assertEqual(expected, result)
 
     def test_convert_currency_float_value_to_cents_str_with_float_input(self):
+        """
+        Dado:
+            - um valor "91.23"
+        Quando:
+            - for chamado convert_currency_float_value_to_cents(value)
+        Então:
+            - o resultado deve ser 9123
+        """
         value = "91.23"
         expected = 9123
         result = convert_currency_float_value_to_cents(value)
         self.assertEqual(expected, result)
 
     def test_convert_currency_float_value_to_cents_none_input(self):
+        """
+        Dado:
+            - um valor None
+        Quando:
+            - for chamado convert_currency_float_value_to_cents(value)
+        Então:
+            - deve ser lançado um FieldError
+        """
         value = None
-        self.assertRaises(FieldError, convert_currency_float_value_to_cents, value)
+        with self.assertRaises(FieldError):
+            convert_currency_float_value_to_cents(value)
 
     def test_convert_currency_float_value_to_cents_random_str_input(self):
+        """
+        Dado:
+            - um valor "random"
+        Quando:
+            - for chamado convert_currency_float_value_to_cents(value)
+        Então:
+            - deve ser lançado um FieldError
+        """
         value = "random"
-        self.assertRaises(FieldError, convert_currency_float_value_to_cents, value)
+        with self.assertRaises(FieldError):
+            convert_currency_float_value_to_cents(value)

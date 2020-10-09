246e52a04c330fdf5b44011a6e9ed8b5793582c5
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2020-10-09 17:56:54 -0300

    Update examples/bank_account/bank_account_remove.py
    Co-authored-by: Rodrigo Castro <rodrigondec@gmail.com>

--- examples/bank_account/bank_account_remove.py
+++ examples/bank_account/bank_account_remove.py
@@ -13,8 +13,6 @@ from zoop_wrapper.constants import MARKETPLACE_ID, ZOOP_KEY
 
 client = ZoopWrapper(marketplace_id=MARKETPLACE_ID, key=ZOOP_KEY)
 
-seller_brian = "0b05a360f4b749498f74e13004c08024"
-brian = client.retrieve_seller(seller_brian)
 
 response = client.remove_bank_account("e85edf6d0add48339e3d8189f615c3b1")
 

b072869c18b2386b41fe07b6a86e62d9de0db4dc
Pedro Regis <pedroalencarregis@hotmail.com>
2020-10-09 12:39:37 -0300

    WIP

---
+++ examples/bank_account/bank_account.py
@@ -0,0 +1,25 @@
+import os
+
+from zoop_wrapper import ZoopWrapper, BankAccount
+from examples.utils import dump_response
+
+"""
+Nesse momento as constantes podem ser criadas no arquivo .py.
+Mas é recomendado utilizar como variável de ambiente em um '.env'
+"""
+from zoop_wrapper.constants import MARKETPLACE_ID, ZOOP_KEY
+
+
+client = ZoopWrapper(marketplace_id=MARKETPLACE_ID, key=ZOOP_KEY)
+
+
+ba = BankAccount(
+    type=BankAccount.SAVING_TYPE,
+    holder_name='Algum Nome',
+    bank_code=,
+    routing_number=
+)
+
+response = client.add_bank_account(ba)
+
+dump_response(response, os.path.basename(__file__).split(".")[0])
